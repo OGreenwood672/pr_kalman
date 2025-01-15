efe7782f354cc7d25ff8dd4a5e1dd45d2e0c960c
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-01-15 15:30:37 +0000

    fixed requirements.txt

--- analyse_multi_journey.py
+++ analyse_multi_journey.py
@@ -518,9 +518,9 @@ def main():
     floor_journey = get_floor_journey(delta_heights_true_displacement)
     print(' -> '.join([str(i) for i in floor_journey]))
 
-    # for i in journey_end_index[::-1]:
-    #     df.loc[i:, [MODEL_DISPLACEMENT]] += df[MODEL_DISPLACEMENT].iloc[i - 1]
-    #     df.loc[i:, [BAROMETER_HEIGHT]] += df[BAROMETER_HEIGHT].iloc[i - 1]
+    for i in journey_end_index[::-1]:
+        df.loc[i:, [MODEL_DISPLACEMENT]] += df[MODEL_DISPLACEMENT].iloc[i - 1]
+        df.loc[i:, [BAROMETER_HEIGHT]] += df[BAROMETER_HEIGHT].iloc[i - 1]
 
     chart(df, MODEL_DISPLACEMENT)
     chart(df, BAROMETER_HEIGHT)
